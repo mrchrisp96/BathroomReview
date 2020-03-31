c2deee15515d18647153a6b615ef39e9aaf9ab30
Christopher Park <basket123rocks@gmail.com>
2020-03-31 00:39:16 -0400

    last commit

--- src/main/java/servlet/bathroomReview.java
+++ src/main/java/servlet/bathroomReview.java
@@ -156,7 +156,7 @@ private void PrintBody (PrintWriter out)
     out.println("");
     out.println("");
     out.println("    <button onclick=\"getScore()\">Submit</button>");
-    out.println("    <p><a href=\"https://bathroomreviewresults.herokuapp.com\">SWE432 Assignment 6</a></p>");
+    out.println("    <p><a href=\"https://bathroomreview.herokuapp.com/assignment6\">SWE432 Assignment 6</a></p>");
     out.println("");
     out.println("    <p><a href=\"https://github.com/mrchrisp96/BathroomReviewResults\">GitHub Executable Code</a></p>");
     out.println("</form>");
